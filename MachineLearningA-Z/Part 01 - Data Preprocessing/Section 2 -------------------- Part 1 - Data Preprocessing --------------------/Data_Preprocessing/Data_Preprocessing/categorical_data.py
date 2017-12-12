665ca9dead2c93508778dce8c08b55dd8546607a
SLPeoples <33819909+SLPeoples@users.noreply.github.com>
2017-12-11 16:50:10 -0800

    Regression

--- MachineLearningA-Z/Part 01 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data_Preprocessing/Data_Preprocessing/categorical_data.py
+++ MachineLearningA-Z/Part 01 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data_Preprocessing/Data_Preprocessing/categorical_data.py
@@ -17,12 +17,12 @@ imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
 # Encoding categorical data
-# Encoding the Independent Variable
+    # Encoding the Independent Variable
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 labelencoder_X = LabelEncoder()
 X[:, 0] = labelencoder_X.fit_transform(X[:, 0])
 onehotencoder = OneHotEncoder(categorical_features = [0])
 X = onehotencoder.fit_transform(X).toarray()
-# Encoding the Dependent Variable
+    # Encoding the Dependent Variable
 labelencoder_y = LabelEncoder()
 y = labelencoder_y.fit_transform(y)
--- MachineLearningA-Z/Part 01 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data_Preprocessing/Data_Preprocessing/data_preprocessing_template.R
+++ MachineLearningA-Z/Part 01 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/Data_Preprocessing/Data_Preprocessing/data_preprocessing_template.R
@@ -2,15 +2,16 @@
 
 # Importing the dataset
 dataset = read.csv('Data.csv')
+  # dataset = dataset[,2:3]
 
 # Splitting the dataset into the Training set and Test set
-# install.packages('caTools')
+  # install.packages('caTools')
 library(caTools)
 set.seed(123)
-split = sample.split(dataset$DependentVariable, SplitRatio = 0.8)
+split = sample.split(dataset$Purchased, SplitRatio = 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
 
 # Feature Scaling
-# training_set = scale(training_set)
-# test_set = scale(test_set)
+  # training_set[,2:3] = scale(training_set[,2:3])
+  # test_set[,2:3] = scale(test_set[,2:3])
